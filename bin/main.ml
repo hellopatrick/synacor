6038d7a73a55d045ffa18413b3f02d87a562cd36
Patrick Rogers <patrick@helloresolven.com>
2017-12-30 09:34:45 -0800

    just renaming.

--- bin/main.ml
+++ bin/main.ml
@@ -3,13 +3,13 @@ open Synacor
 
 let read_input file_name =
   let f channel =
-    let buf = Buffer.create 2 in
+    let buffer = Buffer.create 2 in
     let rec aux l =
-      match In_channel.input_buffer channel buf ~len:2 with
+      match In_channel.input_buffer channel buffer ~len:2 with
       | Some _ ->
-        let contents = Buffer.to_bytes buf in
-        let code = Binary_packing.unpack_unsigned_16_little_endian ~buf:contents ~pos:0 in
-        Buffer.reset buf;
+        let buf = Buffer.to_bytes buffer in
+        let code = Binary_packing.unpack_unsigned_16_little_endian ~buf ~pos:0 in
+        Buffer.reset buffer;
         aux (code::l)
       | None -> Array.of_list_rev l
     in aux []
